57dc8b4e4e882f14437f2452693f470a57b8d131
Elinee Freites <nathalie.freitesmunoz@educa.madrid.org>
2022-04-18 12:21:49 +0200

    Commit desde local con usuario original 2

--- miHolaMundoLocal/src/main/java/com/mycompany/miholamundolocal/Main.java
+++ miHolaMundoLocal/src/main/java/com/mycompany/miholamundolocal/Main.java
@@ -11,7 +11,7 @@ package com.mycompany.miholamundolocal;
 public class Main {
     public static void main(String[] args) {
     System.out.println("Hola Mundo modificado");
-    System.out.println("Yo no soy Pepe");
+    System.out.println("Yo no soy Pepe...");
     }
     
 }
